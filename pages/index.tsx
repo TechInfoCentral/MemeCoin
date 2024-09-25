01404cca5aef31ee7e54c8d76c2fecf08c2e2ca9
TechInfoCentral <dinosoarent@gmail.com>
2024-09-25 19:42:30 +0000

    oineobi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { TokenomicsSection } from "content/TokenomicsSection"
-import { useAirdrop } from "hooks/useAirdrop"
+// import { useAirdrop } from "hooks/useAirdrop"
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
