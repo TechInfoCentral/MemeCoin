f839c58a81808923dc6420f5e3ccc80b1d3313a8
TechInfoCentral <dinosoarent@gmail.com>
2024-09-25 19:50:55 +0000

    h,blkjlgblkjbkjb

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,25 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document"
 
-export default function Document() {
+export default function Document() { // Added space before parentheses
   return (
     <Html>
       <Head>
         {/* <title>AstralFuel</title> */}
         <meta name="title" content="AstralFuel" />
         <meta name="description" content="AstralFuel is a new meme with big dreams" />
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://astralfueltoken.io" />
         <meta property="og:title" content="AstralFuel" />
         <meta property="og:description" content="AstralFuel is a new meme with big dreams" />
         <meta property="og:image" content="https://astralfueltoken.io/assets/social.png" />
+
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://astralfueltoken.io" />
+        <meta property="twitter:url" content="astralfueltoken.io" />
         <meta property="twitter:title" content="AstralFuel" />
         <meta property="twitter:description" content="AstralFuel is a new meme with big dreams" />
         <meta property="twitter:image" content="https://astralfueltoken.io/assets/social.png" />
+        
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
@@ -24,5 +27,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
-}
+  )
+} // Removed extra semicolon
